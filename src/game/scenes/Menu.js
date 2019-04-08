6d96fe31bee62a7a988f90cd970c0c3aa748c75f
Anabel <anabel99.anabel99@gmail.com>
2019-04-08 18:13:51 +0200

    Menu version sin imagenes pero que fucniona (redireccion correcta)

--- src/game/scenes/Menu.js
+++ src/game/scenes/Menu.js
@@ -10,26 +10,26 @@ export default class Menu extends Scene {
     console.log("Starting Menu ...");
     let i = this.add.image(400, 300, 'sky'); //fondo
     console.log(i);
-    
-    /*this.createButton("cosa",400,300,300,100,function(){
-      this.scene.start('MinijuegoOso');
-    })*/
+
+    //Boton oso
+    const btnOso = this.add.text(100, 100, 'Minijuego Oso', { fill: '#0f0' });
+    btnOso.setInteractive();
+    btnOso.on('pointerover', () => this.scene.start('MinijuegoOso'));
+    //----
+    //Boton delfin
+    const btnDlf = this.add.text(300, 100, 'Minijuego Delfin', { fill: '#0f0' });
+    btnDlf.setInteractive();
+    btnDlf.on('pointerover', () => this.scene.start('MinijuegoDelfin'));
+    //----
+    //Boton monos
+    const btnMono = this.add.text(500, 100, 'Minijuego Monos', { fill: '#0f0' });
+    btnMono.setInteractive();
+    btnMono.on('pointerover', () => this.scene.start('MinijuegoMonos'));
+    //----
 
   }
 
   update () {
   }
 
- /* createButton(string,x,y,w,h,callback){
-    var button1 = add.button(x,y,'button',callback,this,2,1,0);
-
-    button1.anchor.setTo(0.5,0.5);
-    button1.width = w;
-    button1.height = h;
-
-    var txt = add.text(button1.x,button1.y,string,{font:"14px Arial",fill:"#fff",align:"center"});
-
-    txt.anchor.setTo(0.5,0.5);
-
-  }*/
 }
