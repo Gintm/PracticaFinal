51e6e8be50c65c3fabc1249fb88ed8f7118717db
Gemma <gemma.torello@hotmail.com>
2019-04-30 19:43:32 +0200

    Oso

--- src/game/scenes/MinijuegoOso.js
+++ src/game/scenes/MinijuegoOso.js
@@ -25,16 +25,16 @@ export default class MinijuegoOso extends Scene {
 
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(400, 150, 'platform').setScale(0.6).refreshBody();
-    platforms.create(600, 350, 'platform').setScale(0.6).refreshBody();
-    platforms.create(50, 550, 'platform').setScale(0.6).refreshBody();
+    platforms.create(400, 150, 'platform').setScale(0.4).refreshBody();
+    platforms.create(600, 350, 'platform').setScale(0.4).refreshBody();
+    platforms.create(50, 550, 'platform').setScale(0.4).refreshBody();
     
 
-    player = this.physics.add.image(400, 0, 'oso');
+    player = this.physics.add.image(400, 600, 'oso');
     player.setScale(0.03);
     player.setCollideWorldBounds(true);
     //player.setBounce(0.2);
-    player.setGravityY(100);
+    player.setGravityY(300);
     //bomb.setVelocity(200, 20);
     
     cursors = this.input.keyboard.createCursorKeys();
@@ -49,7 +49,7 @@ export default class MinijuegoOso extends Scene {
 
   update () {
     cd += 1;
-    if(cursors.left.isDown)
+    if (cursors.left.isDown)
     {
       player.setVelocityX(-160);
     }
@@ -57,16 +57,16 @@ export default class MinijuegoOso extends Scene {
     {
       player.setVelocityX(160);
     }
-    else if (cursors.up.isDown && cd >= 200)
-    {
-      //player.setGravityY(-200);
-      player.setVelocityY(-160);
-      cd = 0;
-    }
     else
     {
       player.setVelocityX(0);
     }
+
+    if (cursors.up.isDown && (player.body.touching.down || player.body.onFloor()))
+    {
+      player.setVelocityY(-380);
+    }
+    
     
   }
 }
