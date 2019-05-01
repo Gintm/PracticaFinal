f404c7525208435272eae0e5f8084a3adccf8b84
Gemma <gemma.torello@hotmail.com>
2019-05-01 14:57:49 +0200

    Oso - Movimiento horizontal plataformas

--- src/game/scenes/MinijuegoOso.js
+++ src/game/scenes/MinijuegoOso.js
@@ -2,7 +2,9 @@ import { Scene } from 'phaser';
 import { platform } from 'os';
 var cursors;
 var player;
-var platforms;
+var platformsf;
+var platformss;
+var platformst;
 var cd;
 
 
@@ -20,26 +22,47 @@ export default class MinijuegoOso extends Scene {
 
     //platforms = this.physics.add.staticGroup();
 
-    //const platforms = this.add.image(400, 200, 'platform');
-    //platforms.setScale(0.6);
+    //platforms.create(400, 150, 'platform').setScale(0.4).refreshBody();
+    //platforms.create(600, 350, 'platform').setScale(0.4).refreshBody();
+    //platforms.create(50, 550, 'platform').setScale(0.4).refreshBody();
+    //platforms.create(400, 550, 'platform').setScale(0.4).refreshBody();
 
-    platforms = this.physics.add.staticGroup();
+    platformsf = this.physics.add.group({immovable: true});
+    platformss = this.physics.add.group({immovable: true});
+    platformst = this.physics.add.group({immovable: true});
 
-    platforms.create(400, 150, 'platform').setScale(0.4).refreshBody();
-    platforms.create(600, 350, 'platform').setScale(0.4).refreshBody();
-    platforms.create(50, 550, 'platform').setScale(0.4).refreshBody();
+    for (var f = 0; f < 3; f++)
+    {
+      var x = Phaser.Math.RND.between(0, 800);
+      
+      platformsf.create(x, 150, 'platform').setScale(0.4);
+    }
+    for (var s = 0; s < 3; s++)
+    {
+      var x = Phaser.Math.RND.between(0, 800);
+      
+      platformss.create(x, 350, 'platform').setScale(0.4);
+    }
+    for (var t = 0; t < 3; t++)
+    {
+      var x = Phaser.Math.RND.between(0, 800);
+      
+      platformst.create(x, 550, 'platform').setScale(0.4);
+    }
+
     
-
     player = this.physics.add.image(400, 600, 'oso');
     player.setScale(0.03);
     player.setCollideWorldBounds(true);
-    //player.setBounce(0.2);
     player.setGravityY(300);
-    //bomb.setVelocity(200, 20);
     
     cursors = this.input.keyboard.createCursorKeys();
 
-    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, platformsf);
+    this.physics.add.collider(player, platformss);
+    this.physics.add.collider(player, platformst);
+
+    
 
     const btnOso = this.add.text(100, 100, 'Volver', { fill: '#0f0' });
     btnOso.setInteractive();
@@ -65,8 +88,10 @@ export default class MinijuegoOso extends Scene {
     if (cursors.up.isDown && (player.body.touching.down || player.body.onFloor()))
     {
       player.setVelocityY(-380);
-    }
+    }    
     
-    
+    platformsf.setVelocityX(100, 0);
+    platformss.setVelocityX(-100, 0);
+    platformst.setVelocityX(100, 0);
   }
 }
