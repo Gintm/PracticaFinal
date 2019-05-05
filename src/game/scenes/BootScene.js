728caf5e333494d4724f6c7022d3320291311e85
Andy <andy.sombris.32@gmail.com>
2019-05-05 19:43:44 +0200

    Hay tanta mierda que no se que sucede

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -4,7 +4,8 @@ import bomb from '@/game/assets/bomb.png';
 import oso from '@/game/assets/oso.png';
 import platform from '@/game/assets/platform.jpg';
 import delfin from '@/game/assets/delfin.png';
-import projectiles from '@/game/assets/projectiles.png';
+import projectile from '@/game/assets/disparo.png';
+import turtle from '@/game/assets/tortuga.png'
 
 export default class BootScene extends Scene {
     constructor() {
@@ -12,12 +13,13 @@ export default class BootScene extends Scene {
     }
 
     preload() {
-        this.load.image('sky', sky)
-        this.load.image('bomb', bomb)
-        this.load.image('oso', oso)
-        this.load.image('platform', platform)
-        this.load.image('delfin', delfin)
-        this.load.spritesheet('dude', projectiles, { frameWidth: 16, frameHeight: 16 });
+        this.load.image('sky', sky);
+        this.load.image('bomb', bomb);
+        this.load.image('oso', oso);
+        this.load.image('platform', platform);
+        this.load.image('delfin', delfin);
+        this.load.image('projectile', projectile);
+        this.load.image('turtle', turtle);
         //this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
     }
 
--- src/game/scenes/MinijuegoDelfin.js
+++ src/game/scenes/MinijuegoDelfin.js
@@ -1,25 +1,11 @@
 import { Scene } from 'phaser';
+//import { Bullet } from '@/game/scenes/BalaDelfin.js';
 
 var cursors;
 var player;
-var Bullet = new Phaser.Class({
-
-    //Extends: Phaser.GameObject.Image,
-
-    initalize:
-
-    //CONSTRUCTOR DE LA BALA
-    function Bullet (scene)
-    {
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'projectile');
-        this.speed = 1;
-        this.born = 0;
-        this.direction = 0;
-        this.xSpeed = 0;
-        this.ySpeed = 0;
-        this.setSize(12, 12, true);
-    }
-})
+var bullets;
+var tortugas;
+var lastFired = 0;
 
 export default class MinijuegoDelfin extends Scene {
   constructor () {
@@ -36,18 +22,66 @@ export default class MinijuegoDelfin extends Scene {
     cursors = this.input.keyboard.createCursorKeys();
 
     //AÑADIR JUGADOR
-    player = this.physics.add.image(100, 100, 'delfin');
+    player = this.physics.add.image(400, 500, 'delfin');
     player.setScale(0.06);
     player.setCollideWorldBounds(true);
+
+    //AÑADIR BALAS
+    
+    var Bullet = new Phaser.Class({
+
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+    function Bullet (scene)
+    {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'projectile');
+        this.speed = Phaser.Math.GetSpeed(400, 1);
+    },
+
+    fire: function (x, y)
+    {
+        this.setPosition(x, y - 50);
+
+        this.setActive(true);
+        this.setVisible(true);
+    },
+
+    update: function (time, delta)
+    {
+        this.y -= this.speed * delta;
+
+        if (this.y < -50)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        } 
+    }
+    });
+
+    bullets = this.add.group({
+      classType: Bullet,
+      maxSize: 15,
+      runChildUpdate: true
+    });
+
+    //AÑADIR TORTUGAS
+    tortugas = this.physics.add.group();
+    
+    var y = Phaser.Math.RND.between(0, 600);
+    tortugas.create(0, y, 'turtle').setScale(0.03);
 
     //BOTON PARA VOLVER AL MENU
     const volver = this.add.text(100, 100, 'Volver', { fill: '#0f0' });
     volver.setInteractive();
     volver.on('pointerup', () => this.scene.start('Menu'));
+
   }
   
-  update () {
+  update (time, delta) {
     
+    //MOVIMIENTO PERSONAJE
     if(cursors.left.isDown)
     {
       player.setVelocityX(-160);
@@ -69,5 +103,21 @@ export default class MinijuegoDelfin extends Scene {
       player.setVelocityX(0);
       player.setVelocityY(0);
     }
+
+    //INPUT DISPARO
+   if (cursors.space.isDown && time > lastFired)
+    {
+        var bullet = bullets.get();
+
+        if(bullet)
+        { 
+          bullet.fire(player.x, player.y);
+          lastFired = time + 50;
+        }
+        
+    }
+
+    //MOVIMIENTO TORTUGAS
+    tortugas.setVelocityX(100);
   }
 }
