2d5cfc64273610e7bd12932bc02db9fc40ae2ed0
Anabel <anabel99.anabel99@gmail.com>
2019-05-06 18:17:17 +0200

    monov1

--- src/game/scenes/MinijuegoMonos.js
+++ src/game/scenes/MinijuegoMonos.js
@@ -7,23 +7,22 @@ export default class MinijuegoMonos extends Scene {
   }
 
   create () {
+    //Preparar escena
     console.log("Starting MinijuegoMonos ...");
-    let i = this.add.image(400, 300, 'sky'); //fondo
+    let i = this.add.image(400, 300, 'fondo_monos').setScale(0.6); //fondo
     console.log(i);
-    
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '24px', fill: '#000' });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    //Añadir jugador
+    mono1 = this.physics.add.image(400, 500, 'mono1');
+    mono1.setScale(0.06);
+    mono2 = this.physics.add.image(400, 500, 'mono2');
+    mono2.setScale(0.06);
 
 
-//    const bomb = this.physics.add.image(400, 200, 'bomb');
-//    bomb.setCollideWorldBounds(true);
-//    bomb.body.onWorldBounds = true; // enable worldbounds collision event
-//    bomb.setBounce(1);
-//    bomb.setVelocity(200, 20);
 
-const btnOso = this.add.text(100, 100, 'Volver', { fill: '#0f0' });
-    btnOso.setInteractive();
-    btnOso.on('pointerup', () => this.scene.start('Menu'));
-
-  }
 
   update () {
   }
