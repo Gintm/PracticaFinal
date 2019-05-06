4a150543a49d885ba3820aa5c8762cbdcdd232e1
Anabel <anabel99.anabel99@gmail.com>
2019-05-06 18:05:24 +0200

    Import de imagenes

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -7,6 +7,12 @@ import delfin from '@/game/assets/delfin.png';
 import projectile from '@/game/assets/disparo.png';
 import turtle from '@/game/assets/tortuga.png';
 import fondo_oso from '@game/assets/fondo_oso.png';
+import mono2 from '@game/assets/2.png';
+import coco from '@game/assets/coco.png';
+import fondo_delfin from '@game/assets/fondo_delfin.png';
+import fondo_monos from '@game/assets/fondo_monos.png';
+import mono1 from '@game/assets/mono.png';
+import platanos from '@game/assets/platanos7.png';
 
 export default class BootScene extends Scene {
     constructor() {
@@ -22,6 +28,12 @@ export default class BootScene extends Scene {
         this.load.image('projectile', projectile);
         this.load.image('turtle', turtle);
         this.load.image('fondo_oso', fondo_oso)
+        this.load.image('mono2', mono2);
+        this.load.image('coco', coco);
+        this.load.image('fondo_delfin', fondo_delfin);
+        this.load.image('fondo_monos', fondo_monos);
+        this.load.image('mono1', mono1);
+        this.load.image('platanos', platanos)
         //this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
     }
 
